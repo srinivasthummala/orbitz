65175500027fb3439055c0f03702943fdb96c7d0
Srinivas Thummala <Srinivas.Thummala@infostretch.com>
2018-03-21 11:07:19 +0530

    updated testng dependency in pom.xml

--- src/test/java/com/orbitz/tests/FlightBookingTest.java
+++ src/test/java/com/orbitz/tests/FlightBookingTest.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
