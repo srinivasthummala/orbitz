ee49c6d3a00fae28899d52a9664bb1e926b80524
Srinivas Thummala <Srinivas.Thummala@infostretch.com>
2018-03-21 17:26:08 +0530

    updated listener

--- src/main/java/com/orbitz/flights/ListenerTestNG.java
+++ src/main/java/com/orbitz/flights/ListenerTestNG.java
@@ -6,39 +6,33 @@ import org.testng.ITestResult;
 
 public class ListenerTestNG implements ITestListener {
 
-	@Override
+	
 	public void onFinish(ITestContext arg0) {
 		System.out.println("Completed executing test " + arg0.getName());
 	}
 
-	@Override
 	public void onStart(ITestContext arg0) {
 		System.out.println("About to begin executing Test " + arg0.getName());		
 	}
 
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void onTestFailure(ITestResult arg0) {
 	    System.out.println("The name of the testcase failed is :"+arg0.getName());					
 		
 	}
 
-	@Override
 	public void onTestSkipped(ITestResult arg0) {
 	    System.out.println("The name of the testcase Skipped is :"+arg0.getName());						
 	}
 
-	@Override
 	public void onTestStart(ITestResult arg0) {
 	    System.out.println(arg0.getName()+" testcase started");							
 	}
 
-	@Override
 	public void onTestSuccess(ITestResult arg0) {
 	    System.out.println("The name of the testcase passed is :"+arg0.getName());							
 	}
