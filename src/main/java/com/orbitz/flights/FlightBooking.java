b21e71786e0c360d8134cedcd8bafbbedb68c7be
Srinivas Thummala <Srinivas.Thummala@infostretch.com>
2018-03-23 11:30:43 +0530

    explicit wait added

--- src/main/java/com/orbitz/flights/FlightBooking.java
+++ src/main/java/com/orbitz/flights/FlightBooking.java
@@ -10,11 +10,11 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FlightBooking {
-	
-	public WebDriverWait wait;
+
+	public static WebDriverWait wait;
 	WebDriver driver;
 
-	public FlightBooking(WebDriver driver){
+	public FlightBooking(WebDriver driver) {
 		this.driver = driver;
 		wait = new WebDriverWait(driver, 30);
 
@@ -22,12 +22,22 @@ public class FlightBooking {
 
 	// click Flights from menu items
 	public void clickFlightsFromMenu() {
-		driver.findElement(By.xpath("//*[contains(@id, 'tab-flight-tab')]")).click();
+		WebElement menuFlights = driver.findElement(By.xpath("//*[contains(@id, 'tab-flight-tab')]"));
+		if (isClickable(menuFlights)) {
+			menuFlights.click();
+		} else {
+			System.out.println("element is not clickable :" + menuFlights);
+		}
 	}
 
 	// select one-way trip type from radio buttons
 	public void selectTripType() {
-		driver.findElement(By.xpath("//input[contains(@id, 'flight-type-one-way')][@type='radio']/..")).click();
+		WebElement radioOneWay = driver.findElement(By.xpath("//input[contains(@id, 'flight-type-one-way')][@type='radio']/.."));
+		if (isClickable(radioOneWay)) {
+			radioOneWay.click();
+		} else {
+			System.out.println("element is not clickable :" + radioOneWay);
+		}
 	}
 
 	// Enter text into Flying from
@@ -51,28 +61,32 @@ public class FlightBooking {
 		wait.until(ExpectedConditions.elementToBeClickable(destinationValue)).click();
 	}
 
-	//Click  departing date for select date from calendar
-	public void clickDepart(){
+	// Click departing date for select date from calendar
+	public void clickDepart() {
 		String departXpath = "//span[contains(text(), 'Departing')]"
-							 + "/following-sibling::input[starts-with(@id, 'flight-departing')]";
-		WebElement txtDate = driver.findElement(By.xpath(departXpath));
-		txtDate.clear();
-		txtDate.click();
+				+ "/following-sibling::input[starts-with(@id, 'flight-departing')]";
+		WebElement dateDepat = driver.findElement(By.xpath(departXpath));
+		if(isClickable(dateDepat)){
+			dateDepat.clear();
+			dateDepat.click();
+		}else{
+			System.out.println("element is not clickable :"+dateDepat);
+		}
 	}
-	
+
 	// Select date from calendar
 	public void selectDepartDate(String monthValue, String dayValue) {
 		String monthsTitleXpath = "//div[@class='datepicker-cal-month']/table[@class='datepicker-cal-weeks']/caption";
 		List<WebElement> lstMonthsTable = driver.findElements(By.xpath(monthsTitleXpath));
-		
-		for (int i=1; i<=lstMonthsTable.size(); i++) {
-			String monthText = lstMonthsTable.get(i-1).getText();
-			if (monthText.equalsIgnoreCase(monthValue))	{
-				String daysXpath = "//div[@class='datepicker-cal-month']["+i+"]/table[@class='datepicker-cal-weeks']"
-								+ "//td/button[@class='datepicker-cal-date']";
+
+		for (int i = 1; i <= lstMonthsTable.size(); i++) {
+			String monthText = lstMonthsTable.get(i - 1).getText();
+			if (monthText.equalsIgnoreCase(monthValue)) {
+				String daysXpath = "//div[@class='datepicker-cal-month'][" + i
+						+ "]/table[@class='datepicker-cal-weeks']" + "//td/button[@class='datepicker-cal-date']";
 				List<WebElement> lstDates = driver.findElements(By.xpath(daysXpath));
 				for (WebElement date : lstDates) {
-					
+
 					if (date.getText().contains(dayValue)) {
 						((JavascriptExecutor) driver).executeScript("arguments[0].click();", date);
 
@@ -80,11 +94,11 @@ public class FlightBooking {
 					}
 				}
 			}
-		
+
 		}
 		Actions builder = new Actions(driver);
 		String btnNextXpath = "//div[@class='datepicker-dropdown']/div[@class='datepicker-cal']"
-							+ "/button[@class='datepicker-paging datepicker-next btn-paging btn-secondary next']";
+				+ "/button[@class='datepicker-paging datepicker-next btn-paging btn-secondary next']";
 		WebElement btnNext = driver.findElement(By.xpath(btnNextXpath));
 		builder.moveToElement(btnNext).click().perform();
 		try {
@@ -93,28 +107,37 @@ public class FlightBooking {
 			e.printStackTrace();
 		}
 		selectDepartDate(monthValue, dayValue);
-		
-	}
-	
-	//Click search button
-	public void clickSearchButton(){
-		WebElement btnSearch = driver.findElement(By.xpath("//button[contains(@class, 'btn-action')]/span[text()='Search']"));
-		((JavascriptExecutor) driver).executeScript("arguments[0].click();", btnSearch);	
 
 	}
-	
-	//find and return list of flights available
-	public List<WebElement> listOfFlights(){
-		List<WebElement> flightsList = driver.findElements(By.xpath("//div[@id='flight-listing-container']/ul/li/h3/.."));
+
+	// Click search button
+	public void clickSearchButton() {
+		WebElement btnSearch = driver
+				.findElement(By.xpath("//button[contains(@class, 'btn-action')]/span[text()='Search']"));
+		((JavascriptExecutor) driver).executeScript("arguments[0].click();", btnSearch);
+
+	}
+
+	// find and return list of flights available
+	public List<WebElement> listOfFlights() {
+		List<WebElement> flightsList = driver
+				.findElements(By.xpath("//div[@id='flight-listing-container']/ul/li/h3/.."));
 		return flightsList;
 	}
 	
-	//Quit driver 
-	public void quitBrowser(){
+	//check element is clickable or not
+	public static boolean isClickable(WebElement webe) {
+		try {
+			wait.until(ExpectedConditions.elementToBeClickable(webe));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	// Quit driver
+	public void quitBrowser() {
 		driver.quit();
 	}
-	
-
-	
 
 }
